3abe6fc9c21e53c4b087a77de733cfe9a5f18c93
david barkhuizen <david.barkhuizen@gmail.com>
2024-05-02 16:17:56 +0200

    test_that_list_all_currencies_can_be_filtered_by_currency

--- rest_api/tests/test_specification.py
+++ rest_api/tests/test_specification.py
@@ -7,14 +7,25 @@ class CountrySchema(Schema):
     alpha3 = fields.Str(required=True)   
     currencies = fields.List(fields.String(), required=True)
 
+PATH_ROOT = '/countries/'
+
 class TestOverallServiceSpecification(TestCase):
 
     def test_it_can_list_all_countries_providing_at_least_country_name_alpha2_alpha_and_currencies(self):
-        response = self.client.get('/countries/')
-
-        validator = CountrySchema()
+        
+        response = self.client.get(PATH_ROOT)
 
         self.assertTrue(len(response.data) > 0)
 
-        self.assertTrue(sum (x for x in response.data if not validator.validate(x)) == 0)
+        country_validator = CountrySchema()
+        self.assertTrue(sum (x for x in response.data if not country_validator.validate(x)) == 0)
         
+    def test_that_list_all_currencies_can_be_filtered_by_currency(self):
+
+        alpha3 = 'gbp'
+        url = f'{PATH_ROOT}?currencies__alpha3={alpha3}'
+        response = self.client.get(url)
+
+        self.assertTrue(len(response.data) == 1)
+        self.assertTrue(alpha3 in response.data[0]['currencies'])
+
