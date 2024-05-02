a1a40ca356122d369b5e44d147f09d5d471b08c0
david barkhuizen <david.barkhuizen@gmail.com>
2024-05-02 06:20:07 +0200

    wip

--- docker/build_intermediate.sh
+++ docker/build_intermediate.sh
@@ -1,19 +1,25 @@
 #! /usr/bin/env bash
 
+echo
+echo "python venv"
 python3 -m venv .venv
-
 . .venv/bin/activate
 
+echo
 echo "venv: $(which python)"
 
+echo
 echo "installing pip"
 python -m pip install --upgrade "pip==24.0"
 
+echo
 echo "installing pip-tools"
 python -m pip install "pip-tools==7.4.1"
 
+echo
 echo "compiling requirements.txt from requirements.in"
 pip-compile requirements.in
 
+echo
 echo "installation of project dependencies from requirements.txt"
 python -m pip install -r requirements.txt
--- rest_api/countries/serializers.py
+++ rest_api/countries/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
-from countries.models import Country
-    
+from countries.models import Country, Currency
+
+class CurrencySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Currency
+
+    def to_representation(self, instance: Currency):
+        return instance.alpha3
+
 class CountrySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Country
         fields = ['alpha2', 'alpha3', 'name', 'currencies']
+    
+    currencies = CurrencySerializer(read_only=True, many=True)
