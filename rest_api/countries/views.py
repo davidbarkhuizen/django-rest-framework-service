b5dbf706fee9171cb08a7f696e27765731412915
david barkhuizen <david.barkhuizen@gmail.com>
2024-05-02 12:50:13 +0200

    filtering now using proper infrastructure

--- rest_api/countries/views.py
+++ rest_api/countries/views.py
@@ -1,35 +1,29 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
 from rest_framework.response import Response
 from countries.models import Country
 from countries.serializers import CountrySerializer
 from django.utils.timezone import now
 
-def list_countries_query(currency_alpha3: str = None):
-
-    query = Country.objects.exclude(deleted_date__isnull=False)
-
-    if currency_alpha3 is not None:
-        currency_alpha3 = currency_alpha3.lower()
-
-    if currency_alpha3:
-        query = query.filter(currencies__alpha3=currency_alpha3)
-
-    query = query.order_by('-alpha2')
-
-    return query
-
 class CountryViewSet(viewsets.ModelViewSet):
 
-    def list(self, request):
+    serializer_class = CountrySerializer
 
-        currency_filter = request.query_params.get('currency', None)
-        q = list_countries_query(currency_filter)
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filterset_fields = [ 'currencies__alpha3' ]
 
-        serializer = CountrySerializer(q, many=True)
-        
-        return Response(serializer.data)
+    #search_fields = [ 'currencies_alpha3' ]
+
+    def get_queryset(self):
+
+        query = Country.objects.exclude(deleted_date__isnull=False)
+
+        query = query.order_by('alpha2')
+
+        return query
 
     def retrieve(self, request, pk=None):
+                
         return Response(
             CountrySerializer(
                 Country.objects.get(pk=pk)
